e89090da0629d58fb33de0cab8c55fa3b172fd24
Hannibal <86764980+hannibally@users.noreply.github.com>
2022-08-11 11:28:53 +0300

    Create Insurance.java

---
+++ Insurance.java
@@ -0,0 +1,42 @@
+package com.insurancemanagementsystem;
+
+import java.util.Date;
+
+public abstract class Insurance {
+	
+	private String insuranceName;
+	private double insurancePrice;
+	private Date insuranceDate;
+	
+	public abstract void calculate();
+
+	
+	// Getter and Setter
+	public String getInsuranceName() {
+		return insuranceName;
+	}
+
+	public void setInsuranceName(String insuranceName) {
+		this.insuranceName = insuranceName;
+	}
+
+	public double getInsurancePrice() {
+		return insurancePrice;
+	}
+
+	public void setInsurancePrice(double insurancePrice) {
+		this.insurancePrice = insurancePrice;
+	}
+
+	public Date getInsuranceDate() {
+		return insuranceDate;
+	}
+
+	public void setInsuranceDate(Date insuranceDate) {
+		this.insuranceDate = insuranceDate;
+	}
+	
+	
+	
+
+}
