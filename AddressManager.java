24a2504cbcd4b661d5f21f2266a0a9b69c946410
Hannibal <86764980+hannibally@users.noreply.github.com>
2022-08-11 11:25:38 +0300

    Create AddressManager.java

---
+++ AddressManager.java
@@ -0,0 +1,12 @@
+package com.insurancemanagementsystem;
+
+public class AddressManager {
+	
+	public static void addAddress(User user, Address address) {
+		user.getAddressList().add(address);
+	}
+	public static void deleteAddress(User user, Address address) {
+		user.getAddressList().remove(address);
+	}
+
+}
